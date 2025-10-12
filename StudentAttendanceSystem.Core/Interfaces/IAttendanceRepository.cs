dee8493f738fd0070a1a680eddc671e83c43d818
jophez <jophez@users.noreply.github.com>
2025-10-12 11:52:27 +0800

    Add files via upload

---
+++ StudentAttendanceSystem.Core/Interfaces/IAttendanceRepository.cs
@@ -0,0 +1,17 @@
+﻿using StudentAttendanceSystem.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAttendanceSystem.Core.Interfaces
+{
+    public interface IAttendanceRepository
+    {
+        Task<bool> RecordAttendanceAsync(int studentId, AttendanceType type, string? notes = null);
+        Task<List<AttendanceRecord>> GetAttendanceByStudentIdAsync(int studentId);
+        Task<StudentAttendanceStatus?> GetStudentAttendanceStatusAsync(int studentId);
+        Task<AttendanceValidationResult> ValidateAttendanceActionAsync(int studentId, AttendanceType proposedType);
+    }
+}
